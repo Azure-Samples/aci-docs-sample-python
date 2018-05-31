f7c0c2719eae158e90bb14cf6230f33a87d4c63e
mmacy <marsma@microsoft.com>
2018-05-30 17:13:07 -0700

    Task-based container create

--- src/aci_docs_sample.py
+++ src/aci_docs_sample.py
@@ -1,6 +1,7 @@
 import random
 import string
 import time
+import sys
 from os import environ
 from azure.common.client_factory import get_client_from_auth_file
 from azure.mgmt.resource import ResourceManagementClient
@@ -8,12 +9,15 @@ from azure.mgmt.resource.resources.models import ResourceGroup
 from azure.mgmt.containerinstance import ContainerInstanceManagementClient
 from azure.mgmt.containerinstance.models import (ContainerGroup,
                                                  Container,
-                                                 ContainerPort,
-                                                 Port,
-                                                 IpAddress,
-                                                 ResourceRequirements,
-                                                 ResourceRequests,
                                                  ContainerGroupNetworkProtocol,
+                                                 ContainerGroupRestartPolicy,
+                                                 ContainerPort,
+                                                 EnvironmentVariable,
+                                                 IpAddress,
+                                                 Logs,
+                                                 Port,
+                                                 ResourceRequests,
+                                                 ResourceRequirements,
                                                  OperatingSystemTypes)
 
 # Support input() on Python 2.x
@@ -56,10 +60,10 @@ def main():
     resource_group = resclient.resource_groups.get(resource_group_name)
 
     # Demonstrate various container group operations
-    create_container_group(aciclient, resource_group, container_group_name, container_image_app)
-    create_container_group_multi(aciclient, resource_group, multi_container_group_name, container_image_app, container_image_sidecar)
-    #run_task_based_container(aciclient, resource_group, task_container_group_name, container_image_taskbased, None)
-    list_container_groups(aciclient, resource_group)
+    #create_container_group(aciclient, resource_group, container_group_name, container_image_app)
+    #create_container_group_multi(aciclient, resource_group, multi_container_group_name, container_image_app, container_image_sidecar)
+    run_task_based_container(aciclient, resource_group, task_container_group_name, container_image_taskbased, None)
+    #list_container_groups(aciclient, resource_group)
     #print_container_group_details()
 
     input("Press ENTER to delete all resources created by this sample: ")
@@ -141,6 +145,67 @@ def create_container_group_multi(aci_client, resource_group, container_group_nam
 
     print("Once DNS has propagated, container group '{0}' will be reachable at http://{1}".format(container_group_name, container_group.ip_address.fqdn))
 
+def run_task_based_container(aci_client, resource_group, container_group_name, container_image_name, start_command_line = None):
+    """Creates a container group with a single task-based container who's
+       restart policy is 'Never'. If specified, the container runs a custom
+       command line at startup.
+
+    Arguments:
+        aci_client {azure.mgmt.containerinstance.ContainerInstanceManagementClient} -- An authenticated container instance management client.
+        resource_group {azure.mgmt.resource.resources.models.ResourceGroup} -- The resource group in which to create the container group.
+        container_group_name {str} -- The name of the container group to create.
+        container_image_name {str} -- The container image name and tag, for example 'microsoft\aci-helloworld:latest'.
+        start_command_line {str} -- The command line that should be executed when the container starts. This value can be None.
+    """
+
+    # If a start command wasn't specified, use a default
+    if start_command_line is None:
+        start_command_line = "python wordcount.py http://shakespeare.mit.edu/romeo_juliet/full.html"
+
+    # Configure some environment variables in the container which the
+    # wordcount.py or other script can read to modify its behavior.
+    env_var_1 = EnvironmentVariable('NumWords', '5')
+    env_var_2 = EnvironmentVariable('MinLength', '8')
+
+    print("Creating container group '{0}' with start command '{1}'".format(container_group_name, start_command_line))
+
+    # Configure the container
+    container_resource_requests = ResourceRequests(memory_in_gb = 1, cpu = 1.0)
+    container_resource_requirements = ResourceRequirements(requests = container_resource_requests)
+    container = Container(name = container_group_name,
+                          image = container_image_name,
+                          resources = container_resource_requirements,
+                          command = start_command_line.split(),
+                          environment_variables = [env_var_1, env_var_2])
+
+    # Configure the container group
+    group = ContainerGroup(location = resource_group.location,
+                           containers = [container],
+                           os_type = OperatingSystemTypes.linux,
+                           restart_policy = ContainerGroupRestartPolicy.never)
+
+    # Create the container group
+    result = aci_client.container_groups.create_or_update(resource_group.name, container_group_name, group)
+
+    # Wait for the container create operation to complete. The operation is
+    # "done" when the container group provisioning state is one of:
+    # Succeeded, Canceled, Failed
+    while result.done() == False:
+        sys.stdout.write('.')
+        time.sleep(1)
+
+    # Get the provisioning state of the container group.
+    container_group = aci_client.container_groups.get(resource_group.name, container_group_name)
+    if str(container_group.provisioning_state).lower() == 'succeeded':
+        print("\nCreation of container group '{}' succeeded.".format(container_group_name))
+    else:
+        print("\nCreation of container group '{}' failed. Provisioning state is: {}".format(container_group_name, container_group.provisioning_state))
+
+    # Get the logs for the container
+    logs = aci_client.container_logs.list(resource_group.name, container_group_name, container.name)
+
+    print("Logs for container '{0}':".format(container_group_name))
+    print("{0}".format(logs.content))
 
 def list_container_groups(aci_client, resource_group):
    """Lists the container groups in the specified resource group.
